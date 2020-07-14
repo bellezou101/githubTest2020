59a38426b3d55aa7a93c5e3210ec1bba22186768
Belle Zou <bellezou101@yahoo.com>
2020-07-14 12:01:50 -0400

    Initial Commit

---
+++ githubTest2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubTest2020
+//
+//  Created by Belle  Zou  on 7/14/20.
+//  Copyright © 2020 Belle  Zou . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
